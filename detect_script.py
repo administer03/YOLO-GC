65e7c0043ce99926a7838a80f23d830b22be238d
administer03 <news1976@hotmail.com>
2024-03-15 14:40:15 +0700

    Fix bugs of large input images

--- detect_script.py
+++ detect_script.py
@@ -1,7 +1,6 @@
 import os
 
 imgsize = 1024
-source = "../running_data/sample_fits/"
 project_name = "./runs/detect"
 conf_thres = 0.25
 iou_thres = 0.45
@@ -9,9 +8,21 @@ iou_thres = 0.45
 # Full image output filter
 out_filter = 'Linear'
 
-# weight_ = "./pretrain_weight/later_train_weight/vanila_yolo_n.pt"
+# """ old exp """
+# weight_ = "/home/taned/nas/detection/stacked_conv_noskip_main_for_training/pretrain_weight/ignore_weights/yolo_best_s.pt"
+# yologc_weight = '/home/taned/nas/detection/stacked_conv_noskip_main_for_training/pretrain_weight/ignore_weights/FE_stacked_convns_s_3.pt'
+# source = "../running_data/sample_fits/"
+
+
+""" new dataset """
 weight_ = "./pretrain_weight/later_train_weight/full_n.pt"
 yologc_weight = './pretrain_weight/later_train_weight/YOLOGC_n.pt'
+source = "../running_data/sample_fits/lastest_full_fits/"
+
+
+
+# weight_ = "./pretrain_weight/later_train_weight/full_m.pt"
+# yologc_weight = './pretrain_weight/later_train_weight/YOLOGC_m.pt'
 
 # weight_ = "./pretrain_weight/ignore_weights/yolo_best_s.pt"
 # yologc_weight = './pretrain_weight/ignore_weights/FE_stacked_convns_s_3.pt'
@@ -19,9 +30,11 @@ yologc_weight = './pretrain_weight/later_train_weight/YOLOGC_n.pt'
 os.system("python detect_modify.py --imgsz {} \
 --source {} --conf-thres {} --iou-thres {}  \
 --weights {} --save-conf --save-txt \
---device 0 --exist-ok --raw-type 0 \
---name testing --project {} --yologc_weight {} --out_filter {}".format(imgsize, source,
+--device 0 --raw-type 0 --large-img True --debug False \
+--name 814_infer_fully --project {} --yologc_weight {} --out_filter {}".format(imgsize, source,
                                                                        conf_thres, iou_thres,
                                                                        weight_, project_name,
                                                                        yologc_weight,
                                                                        out_filter))
+
+# --exist-ok
